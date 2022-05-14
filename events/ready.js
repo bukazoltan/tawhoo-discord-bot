582f6477bfcc23859d94368c0e63e1fc73f38f9c
bukazoltan <bukazoltan@gmail.com>
2022-05-14 16:59:39 +0200

    permission fix

--- events/ready.js
+++ events/ready.js
@@ -1,37 +1,8 @@
-const { guildId, Tawhoo } = require("./../config.json");
 
 module.exports = {
   name: "ready",
   once: true,
   async execute(client) {
-    const permissions = Tawhoo.adminIDs.map((id) => {
-      return {
-        id,
-        type: "USER",
-        permission: true,
-      };
-    });
-
     console.log(`Ready! Logged in as ${client.user.tag}`);
-
-    const setPermission = async (commandId, permissions) => {
-      const command = await client.guilds.cache
-        .get(guildId)
-        ?.commands.fetch(commandId);
-      command.permissions.set({ permissions });
-      console.log(`A ${commandId} parancs jogosultsága felülírva.`);
-    };
-    // DWH
-    setPermission("932217809499455499", permissions);
-    setPermission("932217809499455501", permissions);
-    setPermission("932217809499455502", permissions);
-    // WHOAnon
-    /*setPermission("941036395441557520", permissions);
-    setPermission("941036395441557519", permissions);
-    setPermission("941036395441557517", permissions);*/
-    const commands = await client.guilds.cache.get(guildId)?.commands.fetch();
-    commands.forEach((command) =>
-      console.log(`${command.name} - ${command.id}`)
-    );
   },
 };
