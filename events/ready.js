3fb3ce629843667da73461fcd3f53b18acbc2eb2
Zoltán Buka <bukazoltan@gmail.com>
2023-07-09 19:16:56 +0300

    fixed permissions

--- events/ready.js
+++ events/ready.js
@@ -4,27 +4,6 @@ module.exports = {
   name: "ready",
   once: true,
   async execute(client) {
-    const permissions = Tawhoo.adminIDs.map((id) => {
-      return {
-        id,
-        type: "USER",
-        permission: true,
-      };
-    });
-
-    console.log(`Ready! Logged in as ${client.user.tag}`);
-
-    const setPermission = async (commandId, permissions) => {
-      const command = await client.guilds.cache
-        .get(guildId)
-        ?.commands.fetch(commandId);
-      command.permissions.set({ permissions });
-      console.log(`A ${commandId} parancs jogosultsága felülírva.`);
-    };
-    // DWH
-    setPermission("932217809499455499", permissions);
-    setPermission("932217809499455501", permissions);
-    setPermission("932217809499455502", permissions);
     // WHOAnon
     /*setPermission("941036395441557520", permissions);
     setPermission("941036395441557519", permissions);
